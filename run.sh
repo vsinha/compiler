7cc30515712ca55f65138b96b0512976e9e331fb
Viraj Sinha <virajosinha@gmail.com>
2014-09-06 15:43:19 -0400

    added updated run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-java -cp lib/antlr.jar:classes/ Micro test.micro 
+java -cp lib/antlr.jar:classes/ Micro testcases/step2/input/test1.micro 
+
+java -cp lib/antlr.jar:classes/ Micro testcases/step2/input/test2.micro 
+
+java -cp lib/antlr.jar:classes/ Micro testcases/step2/input/test3.micro 
+
+java -cp lib/antlr.jar:classes/ Micro testcases/step2/input/test4.micro 
+
+java -cp lib/antlr.jar:classes/ Micro testcases/step2/input/test5.micro 
+
+java -cp lib/antlr.jar:classes/ Micro testcases/step2/input/test6.micro 
