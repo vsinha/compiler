9d18c677f573971ab6389e2093a5b45e11013ef1
Viraj Sinha <virajosinha@gmail.com>
2014-09-30 11:01:32 -0400

    bringing this up to date

--- run.sh
+++ run.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 
-java -cp lib/antlr.jar:classes/ Micro testcases/step2/input/test1.micro 
 
-java -cp lib/antlr.jar:classes/ Micro testcases/step2/input/test2.micro 
 
-java -cp lib/antlr.jar:classes/ Micro testcases/step2/input/test3.micro 
+java -cp lib/antlr.jar:classes/ Micro testcases/step2/input/test${1}.micro 
 
-java -cp lib/antlr.jar:classes/ Micro testcases/step2/input/test4.micro 
+echo ""
 
-java -cp lib/antlr.jar:classes/ Micro testcases/step2/input/test5.micro 
-
-java -cp lib/antlr.jar:classes/ Micro testcases/step2/input/test6.micro 
