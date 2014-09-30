4fcce803b0b7f9fe82a57293811b15f0b76865ac
Viraj Sinha <virajosinha@gmail.com>
2014-09-30 17:41:02 -0400

    fleshed this out and added toString

--- src/Id.java
+++ src/Id.java
@@ -1,11 +1,32 @@
 public class Id {
-    String token;
-    Type type;
-    int size;
+    String name;
+    String type;
+    String value;
 
-    public Id (String token, Type type, int size) {
-        this.token = token;
+    public Id (String name, String type) {
+        this.name = name;
         this.type = type;
-        this.size = size;
+        this.value = null;
     }
+
+    public Id (String name, String type, String value) {
+        this.name = name;
+        this.type = type;
+        this.value = value;
+    }
+
+    public boolean isString() {
+        return type.equals("STRING");
+    }
+
+    @Override
+    public String toString() {
+        if (isString()) {
+          return new String("name " + name + " type " + type 
+                  + " value " + value); 
+        } else {
+          return new String("name " + name + " type " + type); 
+        }
+    }
+
 }
